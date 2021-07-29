b22a2aca0c7d9500ca2e0c08a7e04525dd7dbd12
fcfc2021 <87288821+fcfc2021@users.noreply.github.com>
2021-07-29 11:18:59 +0800

    Add files via upload

---
+++ part6/anecdotes/src/Store.js
@@ -0,0 +1,17 @@
+import { createStore ,combineReducers} from 'redux'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+import notificationReducer from'./reducers/notificationReducer'
+
+
+
+const reducer=combineReducers({
+  anecdotes:anecdoteReducer,
+  filter:filterReducer,
+  notification:notificationReducer
+})
+
+const store = createStore(reducer)
+export default store
+
+
